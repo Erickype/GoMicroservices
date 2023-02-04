052752292187dc51fe853542919d327fd0d35504
Erickype <67160282+Erickype@users.noreply.github.com>
2023-02-04 12:41:17 -0500

    use of the local package

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/Erickype/GoMicroservices/geometry"
 	"rsc.io/quote"
 )
 
@@ -13,4 +14,14 @@ func main() {
 	//At the same time
 	var daysMonth = map[string]int{"Jan": 1, "Feb":2}
 	fmt.Println(daysMonth)
+
+	//uso of local package
+	var length, width float64 = 3, 4
+	res := geometry.Area(length, width)
+
+	fmt.Printf("Area: %.2f\n", res)
+
+	res = geometry.Diagonal(length, width)
+
+	fmt.Printf("Diagonal: %.2f\n", res)
 }
