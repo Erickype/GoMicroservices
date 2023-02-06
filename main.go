76e3108c6c3453a158dfb429e31bc337cabc5a78
Erickype <67160282+Erickype@users.noreply.github.com>
2023-02-06 10:58:22 -0500

    Server http basic

--- main.go
+++ main.go
@@ -2,26 +2,14 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/Erickype/GoMicroservices/geometry"
-	"rsc.io/quote"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello world!!")
-	fmt.Println(quote.Go())
-
-	//At the same time
-	var daysMonth = map[string]int{"Jan": 1, "Feb":2}
-	fmt.Println(daysMonth)
-
-	//uso of local package
-	var length, width float64 = 3, 4
-	res := geometry.Area(length, width)
-
-	fmt.Printf("Area: %.2f\n", res)
-
-	res = geometry.Diagonal(length, width)
-
-	fmt.Printf("Diagonal: %.2f\n", res)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	})
+	log.Println("Web server started!")
+	http.ListenAndServe(":80", nil)
 }
