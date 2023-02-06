9d80add635e2e46dff59fca218fad3dd20455db0
Erickype <67160282+Erickype@users.noreply.github.com>
2023-02-06 11:01:01 -0500

    dynamic route

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+		fmt.Fprintf(w, "Hello, you've requested: %s\n with token: %s\n", r.URL.Path, r.URL.Query().Get("token"))
 	})
 	log.Println("Web server started!")
 	http.ListenAndServe(":80", nil)
