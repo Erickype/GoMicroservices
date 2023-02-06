96f754b4210225fcf2657064b7299135631cefa0
Erickype <67160282+Erickype@users.noreply.github.com>
2023-02-06 11:06:52 -0500

    Serve of static files

--- main.go
+++ main.go
@@ -8,8 +8,12 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n with token: %s\n", r.URL.Path, r.URL.Query().Get("token"))
+		fmt.Fprintf(w, "Hello, you've requested: %s with token: %s\n", r.URL.Path, r.URL.Query().Get("token"))
 	})
+
+	fs := http.FileServer(http.Dir("static/"))
+    http.Handle("/static/", http.StripPrefix("/static/", fs))
+
 	log.Println("Web server started!")
 	http.ListenAndServe(":80", nil)
 }
