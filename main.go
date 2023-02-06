7f792a3583e3ffbf07e0297f839da04f46f43709
Erickype <67160282+Erickype@users.noreply.github.com>
2023-02-06 11:32:38 -0500

    root handler function and route

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -19,10 +20,18 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(response)
 }
 
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Serving home page")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Application up and running")
+}
+
 func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/health", healthHandler)
+
+	r.HandleFunc("/", rootHandler)
 
 	log.Fatal(http.ListenAndServe(":80", r))
 }
