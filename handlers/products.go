77dad47e05726751af4d20c88ac20f6ad0851a23
Erickype <67160282+Erickype@users.noreply.github.com>
2023-05-19 16:17:52 -0500

    AddProduct handler

--- handlers/products.go
+++ handlers/products.go
@@ -21,15 +21,32 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		p.getProducts(rw, r)
 		return
 	}
+	if r.Method == http.MethodPost {
+		p.addProduct(rw, r)
+		return
+	}
 
 	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
 
 func (p *Products) getProducts(rw http.ResponseWriter, _ *http.Request) {
+	p.logger.Println("Handle GET products")
+
 	products := data.GetProducts()
 	err := products.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "Unable to marshal", http.StatusInternalServerError)
 		return
 	}
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	p.logger.Println("Handle POST product")
+	product := &data.Product{}
+	err := product.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal JSON", http.StatusBadRequest)
+		return
+	}
+	data.AddProduct(product)
 }
