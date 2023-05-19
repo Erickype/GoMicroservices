72a444232d65571c6e758fb356e2852565afc816
Erickype <67160282+Erickype@users.noreply.github.com>
2023-05-19 12:25:47 -0500

    Hello world function using response writer and request

--- main.go
+++ main.go
@@ -1,23 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+	http.HandleFunc("/hello", func(writer http.ResponseWriter, request *http.Request) {
 		log.Println("Hello word!!")
 		data, err := io.ReadAll(request.Body)
 		if err != nil {
-			panic(err)
-		}
-		log.Printf("Data: %s\n", data)
-		_, err = writer.Write(data)
-		if err != nil {
+			http.Error(writer, "Oops", http.StatusBadRequest)
 			return
 		}
+		_, _ = fmt.Fprintf(writer, "Hello %s!", data)
 	})
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
