310451b5095c265ec96aa5c0cab3ac8947a87add
Erickype <67160282+Erickype@users.noreply.github.com>
2023-05-20 17:55:59 -0500

    Get handler swagger doc

--- handlers/get.go
+++ handlers/get.go
@@ -5,6 +5,12 @@ import (
 	"net/http"
 )
 
+// swagger:route GET /products products listProducts
+// Returns the list of products
+// responses:
+//  200: productsResponse
+
+// GetProducts returns the list of products from the data store
 func (p *Products) GetProducts(rw http.ResponseWriter, _ *http.Request) {
 	p.logger.Println("Handle GET products")
 
