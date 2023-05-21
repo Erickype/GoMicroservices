5e0a014cffcf86d8c735ca83381fe5c8795c7489
Erickype <67160282+Erickype@users.noreply.github.com>
2023-05-21 12:23:00 -0500

    AddProduct doc

--- handlers/add.go
+++ handlers/add.go
@@ -5,6 +5,17 @@ import (
 	"net/http"
 )
 
+// AddProduct creates a new data.Product in the data source
+//
+//	@Summary		Creates a product
+//	@Description	Creates a product by passing the "data.Product" model
+//	@Tags			products
+//	@Accept			json
+//	@Param			product	body	data.Product	true	"New product"
+//	@Success		202
+//	@Failure		400
+//	@Failure		500
+//	@Router			/products [post]
 func (p *Products) AddProduct(_ http.ResponseWriter, r *http.Request) {
 	p.logger.Println("Handle POST product")
 	product := r.Context().Value(KeyProduct{}).(*data.Product)
